b8c3e542443ba78c37a5291a78fb33fdaafc5320
ytlxy <a739771409@gmail.com>
2020-05-14 16:07:52 +0800

    up data

--- springmvc_web01_start/src/main/java/com/cn/controller/AnnoController.java
+++ springmvc_web01_start/src/main/java/com/cn/controller/AnnoController.java
@@ -1,6 +1,7 @@
 package com.cn.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +17,11 @@ public class AnnoController {
     @RequestMapping("/testRequestBody")
     public String testRequestBody(@RequestBody(required = false) String body){
         System.out.println("执行了:"+body);
+        return "success";
+    }
+    @RequestMapping("/userPathVariable/{id}")
+    public String userPathVariable(@PathVariable("id") Integer id){
+        System.out.println(id);
         return "success";
     }
 }
---
+++ springmvc_web01_start/src/main/webapp/annopath.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: hp
+  Date: 2020/5/14
+  Time: 15:41
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<a href="anno/userPathVariable/100">PathVariable</a>
+</body>
+</html>
